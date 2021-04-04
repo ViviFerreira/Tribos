0c64c7b280a13811ecdfbbc3ab42bc26115c47b5
ViviFerreira <vferreiradeoliveira595@gmail.com>
2021-04-04 19:03:11 -0300

    Alteração de nome nos arquivos

---
+++ back/processLogin.php
@@ -0,0 +1,31 @@
+<?php
+//Verificar se clicou no botão 
+require_once('../classes/usuario.php');
+require_once('conexao.php');
+$u = new Usuario;
+if(isset($_POST['email'])){
+    $email = addslashes($_POST['email']);
+    $senha = addslashes($_POST['senha']);
+
+    //Verificar se está preenchido
+    if(!empty($email) && !empty($senha)){
+        if($u->logar($email, $senha)){
+            header("Location: ../front/explorar.php");
+        }else{
+    ?>
+            <script>
+                alert("Email e/ou senha incorretos!");
+            </script>
+    <?php
+        }
+    }else{
+        ?>
+        <script>
+            alert("Preencha todos os campos!");
+        </script>
+    <?php
+        
+    }
+}
+    
+
---
+++ classes/usuario.php
@@ -0,0 +1,45 @@
+<?php
+
+Class Usuario {
+    private $pdo;
+    public function cadastrar($nome, $email, $senha){
+        global $pdo;
+        // Verificar se e-mail já existe
+        $sql = $pdo->prepare(
+            "SELECT idUsuario FROM usuario WHERE emailUsuario = :e");
+        $sql->bindValue(":e", $email);
+        $sql->execute();
+        if($sql->rowCount() > 0){
+            return false;
+        }
+        else{
+        // Caso não, Cadastrar
+            $sql = $pdo->prepare("INSERT INTO usuario VALUES (DEFAULT, :n, :e, :s)");
+            $sql->bindValue(":n", $nome);
+            $sql->bindValue(":e", $email);
+            $sql->bindValue(":s", md5($senha));
+            $sql->execute();
+            return true;
+        }
+    }
+
+    public function logar($email, $senha){
+        global $pdo;
+        $sql = $pdo->prepare("SELECT idUsuario FROM usuario WHERE emailUsuario = :e AND senhaUsuario = :s");
+        $sql->bindValue(":e", $email);
+        $sql->bindValue(":s", md5($senha));
+        $sql->execute();
+        if($sql->rowCount() > 0){
+        // Está cadastrada
+            $dado = $sql->fetch(PDO::FETCH_ASSOC);
+        // Cria sessão 
+            session_start();
+            $_SESSION['idUsuario'] = $dado['idUsuario']; 
+            return true; //Logado com sucesso
+        }else{
+        // Não está cadastrada
+            return false;
+        }
+
+    }
+}
